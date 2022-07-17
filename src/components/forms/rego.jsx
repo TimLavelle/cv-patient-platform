9ceeef93e31430aa8382f588753d086ffc171393
TimLavelle <tim.lavelle@r3ddstudios.com>
2022-07-17 12:11:55 +1000

    Updated validation for rego

--- src/components/forms/rego.jsx
+++ src/components/forms/rego.jsx
@@ -1,12 +1,12 @@
 import { Fragment, useState, React } from 'react'
-import { Formik, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, formik } from 'formik';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next'
-import { ExclamationCircleIcon, CheckIcon, SelectorIcon, SortAscendingIcon, UsersIcon } from '@heroicons/react/solid'
+import { ExclamationCircleIcon, CheckIcon, SelectorIcon, UsersIcon, BadgeCheckIcon } from '@heroicons/react/solid'
 import { Listbox, Transition } from '@headlessui/react'
 
 const genders = [
-  { id: 0, name: 'Please choose a gender' },
+  { id: "", name: 'Please choose a gender' },
   { id: 1, name: 'Child' },
   { id: 2, name: 'Female' },
   { id: 3, name: 'Male' },
@@ -27,12 +27,11 @@ export function RegForm() {
       .max(4, t('forms.rego.fields.px.numberLength'))
       .required(t('forms.rego.fields.px.numberError')),
 
-    gender: Yup.string()
-      .oneOf(['1', '2'])
-      .required('forms.rego.fields.px.numberError'),
-
     familyName: Yup.string()
       .required(t('forms.rego.fields.px.familyNameError')),
+
+    pxgender: Yup.string()
+      .required(t('forms.rego.fields.px.genderError')),
 
     givenName: Yup.string()
       .required(t('forms.rego.fields.px.givenNameError')),
@@ -44,14 +43,13 @@ export function RegForm() {
       .positive('forms.rego.fields.px.mobile.Error')
   })
 
-  const getProvinces = async() => {
+  const getProvinces = async () => {
     const response = await fetch('http://localhost:3000/api/provinces');
     const provinces = await response.json();
     console.log(provinces);
   }
 
   let provOptions = []
-
 
   return (
     <>
@@ -63,19 +61,29 @@ export function RegForm() {
       <Formik
         initialValues={{
           pxnumber: '',
+          pxgender: '0',
           familyName: '',
           givenName: '',
           pxage: '',
-          mobile: ''
+          mobile: '',
+          province: '',
+          district: '',
+          village: ''
         }}
         validationSchema={registrationSchema}
 
-        onSubmit={values => {
-          console.log('Registration form submitted...', values);
+        onSubmit={async (values, setSubmitting, isSubmitting, isValidating) => {
+          let pxgender = document.getElementById('pxgender').value;
+          const payload = {
+            ...values,
+            pxgender
+          }
+
+          console.log('Registration form submitted...', payload);
         }}
       >
-        {({ handleChange, handleSubmit, errors, values, isValid, touched }) => (
-          <form method="POST">
+        {({ errors, touched }) => (
+          <Form>
             <div className="mt-10 sm:mt-0">
               <div className="md:grid md:grid-cols-3 md:gap-6">
                 <div className="md:col-span-1">
@@ -93,7 +101,10 @@ export function RegForm() {
                     <div className="px-4 py-5 bg-white sm:p-6">
                       <div className="grid grid-cols-6 gap-6">
                         <div className="col-span-6 sm:col-span-3">
-                          <label htmlFor="pxnumber" className="block text-sm font-medium text-gray-700">{t('forms.rego.fields.px.number')}</label>
+                          <label htmlFor="pxnumber" className="block text-sm font-medium text-gray-700">
+                            {t('forms.rego.fields.px.number')}
+                            <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" />
+                          </label>
                           <div className="mt-1 flex rounded-md shadow-sm">
                             <div className="relative flex items-stretch flex-grow focus-within:z-10">
                               <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"><UsersIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /></div>
@@ -110,10 +121,6 @@ export function RegForm() {
                                 </div>
                               }
                             </div>
-                            <button type="button" className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-                              <SortAscendingIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                              <span>Sort</span>
-                            </button>
                           </div>
                           <p className="mt-2 text-sm text-gray-500" id="email-description">
                             {t('forms.rego.fields.px.numberHint')}
@@ -128,15 +135,10 @@ export function RegForm() {
                         </div>
 
                         <div className="col-span-6 sm:col-span-3">
-                          <Listbox
-                            value={selected.id}
-                            name="gender"
-                            id="gender"
-                            onChange={setSelected}
-                          >
+                          <Listbox name='genderSelect' value={selected.id} onChange={setSelected}>
                             {({ open }) => (
                               <>
-                                <Listbox.Label className="block text-sm font-medium text-gray-700">{t('forms.rego.fields.px.gender')}</Listbox.Label>
+                                <Listbox.Label className="block text-sm font-medium text-gray-700">{t('forms.rego.fields.px.gender')} <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" /></Listbox.Label>
                                 <div className="mt-1 relative">
                                   <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                     <span className="block truncate">{selected.name}</span>
@@ -163,7 +165,6 @@ export function RegForm() {
                                             )
                                           }
                                           value={gen}
-
                                         >
                                           {({ selected, active }) => (
                                             <>
@@ -187,19 +188,15 @@ export function RegForm() {
                                       ))}
                                     </Listbox.Options>
                                   </Transition>
-                                  {errors.gender && touched.gender &&
-                                    <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                                      <ExclamationCircleIcon className="h-5 w-5 text-red-500" aria-hidden="true" />
-                                    </div>
-                                  }
                                 </div>
                               </>
                             )}
                           </Listbox>
+                          <Field type="hidden" name="pxgender" id="pxgender" value={selected.name} onChange={setSelected} />
 
-                          {errors.gender && touched.gender &&
+                          {errors.pxgender &&
                             <ErrorMessage
-                              name="gender"
+                              name="pxgender"
                               component="p"
                               className="mt-2 text-sm text-red-600"
                             />
@@ -216,7 +213,7 @@ export function RegForm() {
 
                         <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                           <label htmlFor="familyName" className="block text-sm font-medium text-gray-700">
-                            {t('forms.rego.fields.px.familyName')}
+                            {t('forms.rego.fields.px.familyName')} <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" />
                           </label>
                           <div className="mt-1 flex rounded-md shadow-sm">
                             <div className="relative flex items-stretch flex-grow focus-within:z-10">
@@ -242,7 +239,7 @@ export function RegForm() {
 
                         <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                           <label htmlFor="givenName" className="block text-sm font-medium text-gray-700">
-                            {t('forms.rego.fields.px.givenName')}
+                            {t('forms.rego.fields.px.givenName')} <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" />
                           </label>
                           <div className="mt-1 flex rounded-md shadow-sm">
                             <div className="relative flex items-stretch flex-grow focus-within:z-10">
@@ -268,7 +265,7 @@ export function RegForm() {
 
                         <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                           <label htmlFor="pxage" className="block text-sm font-medium text-gray-700">
-                            {t('forms.rego.fields.px.age')}
+                            {t('forms.rego.fields.px.age')} <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" />
                           </label>
                           <div className="mt-1 flex rounded-md shadow-sm">
                             <div className="relative flex items-stretch flex-grow focus-within:z-10">
@@ -339,58 +336,57 @@ export function RegForm() {
                     <div className='px-4 py-5 bg-white sm:p-6'>
                       <div className='grid grid-cols-6 gap-6'>
                         <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                          <label htmlFor="city" className="block text-sm font-medium text-gray-700">
-                            {t('forms.rego.fields.px.province')}
+                          <label htmlFor="province" className="block text-sm font-medium text-gray-700">
+                            {t('forms.rego.fields.px.province')} <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" />
                           </label>
-                          <input
+                          <Field
                             type="text"
-                            name="city"
-                            id="city"
-                            autoComplete="address-level2"
+                            name="province"
+                            id="province"
                             className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                           />
                         </div>
 
                         <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                          <label htmlFor="region" className="block text-sm font-medium text-gray-700">
-                            {t('forms.rego.fields.px.district')}
+                          <label htmlFor="district" className="block text-sm font-medium text-gray-700">
+                            {t('forms.rego.fields.px.district')} <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" />
                           </label>
-                          <input
+                          <Field
                             type="text"
-                            name="region"
-                            id="region"
-                            autoComplete="address-level1"
+                            name="district"
+                            id="district"
                             className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                           />
                         </div>
 
                         <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                          <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">
-                            {t('forms.rego.fields.px.village')}
+                          <label htmlFor="village" className="block text-sm font-medium text-gray-700">
+                            {t('forms.rego.fields.px.village')} <BadgeCheckIcon className="h-5 w-5 text-blue-500 pl-1 inline" aria-hidden="true" />
                           </label>
-                          <input
+                          <Field
                             type="text"
-                            name="postal-code"
-                            id="postal-code"
-                            autoComplete="postal-code"
+                            name="village"
+                            id="village"
                             className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                           />
                         </div>
-                      </div>
-                      <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button
-                          type="submit"
-                          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                          Save
-                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+              <div className="md:grid md:grid-cols-3 md:gap-6">
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
             </div>
-          </form>
+          </Form>
         )}
       </Formik>
     </>
--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -7,24 +7,24 @@ import { Container } from '@/components/Container'
 import { RegForm } from '@/components/forms/rego'
 
 export default function Home() {
-  const { t, i18n } = useTranslation();
-  const pageTitle = t('global.cv.label') +' - ' + t('global.px.label');
+	const { t, i18n } = useTranslation();
+	const pageTitle = t('global.cv.label') + ' - ' + t('global.px.label');
 
 	return (
-	  <>
-		<Head>
-		  <title>{pageTitle}</title>
-		</Head>
-		<SiteHeader />
-		<main>
-      <Container>
-        <span className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          {t('forms.rego.page.title')}
-        </span>
-        <RegForm />
-      </Container>
-		</main>
-		<Footer />
-	  </>
+		<>
+			<Head>
+				<title>{pageTitle}</title>
+			</Head>
+			<SiteHeader />
+			<main>
+				<Container>
+					<span className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+						{t('forms.rego.page.title')}
+					</span>
+					<RegForm />
+				</Container>
+			</main>
+			<Footer />
+		</>
 	)
 }
